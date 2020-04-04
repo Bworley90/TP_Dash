b78c6e38b5ef6322cc215ea751594887f3eb01b6
Bruce Worley <Bworley90@gmail.com>
2020-04-04 12:42:26 -0400

    1st

---
+++ TP-Dash/Assets/Scripts/CartMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 change;
+    public float speed;
+    [Range(0f, 10f)]
+    public float turnSpeed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetAxisRaw("Vertical") > 0)// Moving Forward
+        {
+            Vector3 newPosition = rb.position + transform.TransformDirection(Vector3.right * speed * Time.deltaTime);
+            rb.MovePosition(newPosition);
+        }
+        else if(Input.GetAxisRaw("Vertical") < 0)
+        {
+            Vector3 newPosition = rb.position + transform.TransformDirection(Vector3.left * speed * Time.deltaTime);
+            rb.MovePosition(newPosition);
+        }
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Rotate(0, turnSpeed, 0);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Rotate(0, -1 * turnSpeed, 0);
+        }
+
+        if(Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ TP-Dash/Assets/Scripts/IsleGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsleGenerator : MonoBehaviour
+{
+    public GameObject isleModel;
+    public float xDistanceBetween;
+    public float zDistanceBetween;
+    public int xDepth;
+    public int zDepth;
+    public bool addRandomness = false;
+
+
+    private Vector3 widthBetweenIsle;
+    private Vector3 heightBetweenRows;
+
+    private void Start()
+    {
+        SetValues();
+        if(addRandomness)
+        {
+            RandomIsleSetup();
+        }
+        else
+        {
+            IsleSetup();
+        }
+    }
+
+    private void IsleSetup()
+    {
+        Vector3 startPos;
+        startPos = transform.position;
+        float xStartPos = startPos.x;
+        for (int i = 0; i < zDepth; i++)
+        {
+            for (int x = 0; x < xDepth; x++)
+            {
+                Instantiate(isleModel, startPos, transform.rotation, transform);
+                startPos += widthBetweenIsle;
+            }
+            startPos += heightBetweenRows;
+            startPos.x = xStartPos;
+           
+        }
+    }
+    private void RandomIsleSetup()
+    {
+        Vector3 startPos;
+        startPos = transform.position;
+        float xStartPos = startPos.x;
+        for (int i = 0; i < zDepth; i++)
+        {
+            for (int x = 0; x < xDepth; x++)
+            {
+                Instantiate(isleModel, startPos, transform.rotation, transform);
+                startPos += widthBetweenIsle;
+            }
+            startPos += heightBetweenRows;
+            startPos.x = xStartPos;
+
+        }
+    }
+
+    private void SetValues()
+    {
+        zDistanceBetween += isleModel.GetComponent<MeshRenderer>().bounds.size.z;
+        xDistanceBetween += isleModel.GetComponent<MeshRenderer>().bounds.size.x;
+        widthBetweenIsle.x = xDistanceBetween;
+        heightBetweenRows.z = zDistanceBetween;
+    }
+
+ 
+}
