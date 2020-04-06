a6a77c869aa852f80b93a2a056ec86841ff139e1
Bruce Worley <Bworley90@gmail.com>
2020-04-05 21:06:35 -0400

    Added TP Spawn to Room

---
+++ TP-Dash/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int tpCollected;
+    public int tpTotal;
+    public int tpSpawned;
+    [Range(1,100)]
+    [Tooltip("Percent to Spawn TP per node")]
+    public int difficulty;
+    public GameObject tp;
+    
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+}
---
+++ TP-Dash/Assets/Scripts/TPSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSpawn : MonoBehaviour
+{
+    private GameManager gm;
+    private float spawnChancePercentage;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        spawnChancePercentage = gm.difficulty;
+        SpawnTP();
+    }
+
+
+
+    private void SpawnTP()
+    {
+        if(gm.tpSpawned < gm.tpTotal)
+        {
+            print(transform.childCount);
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int randomNumber = Random.Range(0, 100);
+                if (randomNumber < spawnChancePercentage)
+                {
+                    Instantiate(gm.tp, transform.GetChild(i).transform);
+                    gm.tpSpawned++;
+                }
+                else
+                {
+                   
+                }
+            }
+        }
+        
+    }
+
+}
