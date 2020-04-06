90d9e624c8b9b8690216ecefca1865926c62f913
Bruce Worley <Bworley90@gmail.com>
2020-04-06 09:59:27 -0400

    Optimzied shelfing
    Removed shelving boxes individually and made them 1 object and assigning materials to faces

--- TP-Dash/Assets/Blender/shelf/ShelfColorChange.cs
+++ TP-Dash/Assets/Blender/shelf/ShelfColorChange.cs
@@ -6,9 +6,31 @@ public class ShelfColorChange : MonoBehaviour
 {
     public MeshRenderer[] boxes;
     public Color[] colorChoices;
+    private Material[] materialCount;
 
 
     private void Start()
+    {
+        BoxReduced();
+    }
+
+    private void BoxReduced()
+    {
+        materialCount = GetComponent<MeshRenderer>().materials;
+        for (int i = 0; i < materialCount.Length; i++)
+        {
+            if(materialCount[i].name != "BaseColor (Instance)")
+            {
+                if(materialCount[i].name != "ShelfColor (Instance)")
+                {
+                    materialCount[i].color = colorChoices[Random.Range(0, colorChoices.Length)];
+                }
+            }
+            
+        }
+    }
+
+    private void ShelfWithBox()
     {
         boxes = GetComponentsInChildren<MeshRenderer>();
         for (int i = 0; i < boxes.Length; i++)
@@ -19,5 +41,8 @@ public class ShelfColorChange : MonoBehaviour
             }
         }
     }
+
+
+
 
 }
