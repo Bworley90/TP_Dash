5b1b39e43cfb30572cf393eb77d3170db45fd62c
Bruce Worley <Bworley90@gmail.com>
2020-04-08 11:35:51 -0400

    Testing AI

---
+++ TP-Dash/Assets/Scripts/NavAgentGoal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentGoal : MonoBehaviour
+{
+    private Transform goal;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        goal = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
