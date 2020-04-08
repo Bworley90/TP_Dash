d2cad8ab80dbb659908600171ab9ac9d88ffcba5
Bworley90 <40043696+Bworley90@users.noreply.github.com>
2020-04-08 15:40:13 -0400

    Added UI update to show TP checked out total

---
+++ TP-Dash/Assets/Scripts/UITpTotal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITpTotal : MonoBehaviour
+{
+
+    private Text text;
+    private GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = gm.numberOfTPCheckedOut.ToString();
+    }
+}
