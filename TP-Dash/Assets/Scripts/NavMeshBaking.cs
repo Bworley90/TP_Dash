6e5db2e16bdcc4303cdb06589e445f9b6bc07095
Bruce Worley <Bworley90@gmail.com>
2020-04-08 13:33:26 -0400

    Fixed AI tracking and added new layer for Non-NaveMesh

---
+++ TP-Dash/Assets/Scripts/NavMeshBaking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NavMeshBaking : MonoBehaviour
+{
+    public NavMeshSurface nms;
+    
+    void Start()  
+    {
+        Invoke("Build", .5f);
+    }
+
+   private void Build()
+    {
+        nms.BuildNavMesh();
+    }
+}
