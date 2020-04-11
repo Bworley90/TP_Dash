6a5cca1eb6ce629838af7ee19e14a5ff043a23a2
Bruce Worley <Bworley90@gmail.com>
2020-04-11 19:06:53 -0400

    Added new model and updated clothing room

---
+++ TP-Dash/Assets/Scripts/ModelColorRandomizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelColorRandomizer : MonoBehaviour
+{
+    public Color[] colors;
+    public string skipMaterial1;
+    public string skipMaterial2;
+    private MeshRenderer mr;
+    public Material[] materialCount;
+
+    private void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        ChangeColors();
+    }
+
+    private void ChangeColors()
+    {
+        materialCount = mr.materials;
+        for (int i = 0; i < materialCount.Length; i++)
+        {
+            if (materialCount[i].name != skipMaterial1 + " (Instance)")
+            {
+                if (materialCount[i].name != skipMaterial2 + " (Instance)")
+                {
+                    materialCount[i].color = colors[Random.Range(0, colors.Length)];
+                }
+                materialCount[i].color = colors[Random.Range(0, colors.Length)];
+            }
+
+        }
+    }
+
+
+
+
+}
