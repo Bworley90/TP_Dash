77aa67616114f06636a0c9edcd8778ec237a643b
Bruce Worley <Bworley90@gmail.com>
2020-04-12 23:25:36 -0400

    Added LawnMower easteregg

---
+++ TP-Dash/Assets/Scripts/SmallLawnMower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallLawnMower : MonoBehaviour
+{
+    private Animator anim;
+    private bool inside;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inside = true;
+        }
+        else
+        {
+            inside = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(inside)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetTrigger("on");
+            }
+        }
+    }
+}
